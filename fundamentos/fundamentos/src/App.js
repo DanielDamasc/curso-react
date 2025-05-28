4eb95f5fe9244d358a44b42c6f5b3497d3004b9d
Daniel Damasceno <dandamasceno04@gmail.com>
2025-05-28 17:30:34 -0300

    criando o primeiro componente

---
+++ fundamentos/fundamentos/src/App.js
@@ -0,0 +1,16 @@
+// components
+import FirstComponent from './components/FirstComponent';
+
+// css
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos do React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ fundamentos/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,10 @@
+const FirstComponent = () => {
+    return (
+        <div>
+            <h1>Primeiro Componente</h1>
+            <p className="text">Meu texto</p>
+        </div>
+    );
+};
+
+export default FirstComponent;
