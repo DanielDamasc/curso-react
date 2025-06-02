04f13f6c50a41e90d8a85a38d03407594dac74c1
Daniel Damasceno <dandamasceno04@gmail.com>
2025-06-02 14:14:28 -0300

    propriedade key

--- avançando/avancando/src/components/ListRender.jsx
+++ avançando/avancando/src/components/ListRender.jsx
@@ -3,12 +3,22 @@ import { useState } from "react"
 const ListRender = () => {
     const [List] = useState(["Maria", "Pedro", "João", "Marta"]);
 
+    const [Users] = useState([
+      {id: 1, nome: "Daniel", age: 20},
+      {id: 2, nome: "Jeff", age: 50},
+    ]);
+
   return (
     <div>
         <ul>
-            {List.map((item) => (
-                <li>{item}</li>
+            {List.map((item, i) => (
+                <li key={i}>{item}</li>
             ))}
+        </ul>
+        <ul>
+          {Users.map((item) => (
+            <li key={item.id}>{item.nome} - {item.age}</li>
+          ))}
         </ul>
     </div>
   )
